a617719a9f7e8148377b838fd92e0af9df96a4b2
Anahit Yeghyan <anahit.yeghyann@gmail.com>
2026-03-14 12:47:50 +0400

    Update settings.py

--- earlyone/settings.py
+++ earlyone/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wb6jj@paxl7xo9rf%#w$3$#h01-yo_znbq9y*3r*qq!uep+5so'
@@ -49,16 +52,15 @@ WSGI_APPLICATION = 'earlyone.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'earlyone_db',
-        'USER': 'postgres',
-        'PASSWORD': 'g7n9guVwG5fTSygz',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
